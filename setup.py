6445c74dea917290df49232741fefa3984b95d76
Brian <bri4nong@gmail.com>
2020-05-10 15:05:06 +0700

    Add requirements

--- setup.py
+++ setup.py
@@ -18,4 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+          'selenium',
+    ],
     python_requires='>=3.6')
