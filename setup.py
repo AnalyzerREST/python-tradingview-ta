d0a94d681c5dbe2938e5e14bbf273bcccdcf2b0d
Brian <bri4nong@gmail.com>
2020-09-30 20:13:51 +0700

    Add set functions. Updated to 3.1.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='tradingview_ta',
-    version='3.0.0',
+    version='3.1.0',
     description="A python module to scrape tradingview's technical analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
