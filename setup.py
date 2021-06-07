4f59427fcd5e1451a6d3ebc16f37b48a6c96853c
Brian <brian@brianthe.dev>
2021-06-07 12:42:08 +0700

    Update username and domain

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setuptools.setup(
     description="Unofficial TradingView technical analysis API wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/deathlyface/python-tradingview-ta',
+    url='https://github.com/brian-the-dev/python-tradingview-ta',
     author='deathlyface',
-    author_email='contact@deathlyf.com',
+    author_email='brian@brianthe.dev',
     packages=['tradingview_ta'],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,8 +23,8 @@ setuptools.setup(
     ],
     python_requires='>=3.5',
     project_urls={
-        'Demo': 'https://tradingview.deathlyf.com',
+        'Demo': 'https://tradingview.brianthe.dev',
         'Documentation': 'https://python-tradingview-ta.readthedocs.io',
-        'Source': 'https://github.com/deathlyface/python-tradingview-ta',
+        'Source': 'https://github.com/brian-the-dev/python-tradingview-ta',
     },
 )
