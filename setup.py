8b2aeea976e0e2a302cbdaa4995285397f3064f4
mohamedraslan85 <75303084+mohamedraslan85@users.noreply.github.com>
2021-07-07 11:58:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tradingview_ta',
-    version='3.2.4',
+    name='tradingview_ta1',
+    version='3.2.5',
     description="Unofficial TradingView technical analysis API wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/brian-the-dev/python-tradingview-ta',
-    author='deathlyface',
+    url='https://github.com/mohamedraslan85/python-tradingview-ta/',
+    author='mraslan',
     author_email='brian@brianthe.dev',
     packages=['tradingview_ta'],
     classifiers=[
@@ -25,6 +25,6 @@ setuptools.setup(
     project_urls={
         'Demo': 'https://tradingview.brianthe.dev',
         'Documentation': 'https://python-tradingview-ta.readthedocs.io',
-        'Source': 'https://github.com/brian-the-dev/python-tradingview-ta',
+        'Source': 'https://github.com/mohamedraslan85/python-tradingview-ta/',
     },
 )
